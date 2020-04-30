c1b107a6b22a13c700d92806f2db34ca5faa9696
mcpdude <mcpdude@gmail.com>
2020-04-30 17:51:04 -0600

    add changes to the db to support new features

--- schema.sql
+++ schema.sql
@@ -4,7 +4,9 @@ create table if not exists articles (
 	title text not null,
 	read int default 0,
 	local_html_path text,
-	interesting int default 0
+	interesting int default 0,
+	visible int default 1,
+	source_id int not null
 );
 
 create table if not exists sources (
