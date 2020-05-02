c3323d9cfb617d73f0eddacc3dbfbbb07fb35d21
mcpdude <mcpdude@gmail.com>
2020-05-01 18:53:49 -0600

    change schema for tables to support training

--- schema.sql
+++ schema.sql
@@ -6,12 +6,16 @@ create table if not exists articles (
 	local_html_path text,
 	interesting int default 0,
 	visible int default 1,
-	source_id int not null
+	source_id int not null,
+	added_timestamp text default (datetime('now')),
+	inferred_interest real default 0.0,
+	blurb text
 );
 
 create table if not exists sources (
 	id integer primary key autoincrement,
 	root_url text not null,
-	name text not null
+	name text not null,
+	date_added text default (datetime('now'))
 );
 
