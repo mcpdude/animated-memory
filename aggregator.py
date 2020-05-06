86cea84f66a9a63124c30cd0fe570998f636f49f
mcpdude <mcpdude@gmail.com>
2020-05-05 22:51:58 -0600

    create new, exciting settings page, with settings

--- aggregator.py
+++ aggregator.py
@@ -27,10 +27,9 @@ here = os.path.dirname(os.path.abspath(__file__))
 
 @view_config(route_name='list', renderer='list.mako')
 def list_view(request):
-	current_articles = request.db.execute('select url from articles')
-	current_articles = [item[0] for item in current_articles.fetchall()]
-
-	rs = request.db.execute('select id, title, url, interesting from articles where read = 0 and visible = 1 limit 50')
+	rs = request.db.execute('select articles_to_show from settings')
+	amount_of_articles = rs.fetchone()[0]
+	rs = request.db.execute('select id, title, url, interesting from articles where read = 0 and visible = 1 limit ?', [amount_of_articles])
 	articles = [dict(id=row[0], title=row[1], url=row[2], interesting=row[3]) for row in rs.fetchall()]
 	return {'articles': articles}
 
@@ -77,6 +76,47 @@ def read_articles(request):
 	articles = [dict(id=row[0], title=row[1], url=row[2], interesting=row[3]) for row in rs.fetchall()]
 	return {'articles': articles}
 
+@view_config(route_name='settings', renderer='settings.mako')
+def settings(request):
+	rs = request.db.execute('select * from settings')
+	# print(rs.fetchall())
+	settings = [dict(name=row[0], articles_to_show=row[1]) for row in rs.fetchall()]
+	print(settings)
+	return {'settings': settings}
+
+@view_config(route_name='change_settings')
+def change_settings(request):
+
+	if request.GET.get('field') == "Articles to Show":
+		if request.GET.get('articles_to_show') == "":
+			print("no amount supplied, defaulting to 1.")
+			request.session.flash("Please provide a value!")
+			value = '1'
+
+		elif int(request.GET.get('articles_to_show')) < 1:
+			request.session.flash("You can't show negative articles")
+			value = '1'
+
+		else:
+			value = request.GET.get('articles_to_show')
+		request.db.execute('update settings set articles_to_show = ?', [value])
+
+	elif request.GET.get('field') == "Change Name":
+		if request.GET.get('name') == "":
+			request.session.flash("Name set to Anon")
+			value = 'Anon'
+
+		else:
+			value = request.GET.get('name')
+
+		request.db.execute('update settings set name = ?', [value])
+
+	request.db.commit()
+	request.session.flash("Settings updated!")
+
+	return HTTPFound(location=request.route_url('settings'))
+
+
 @view_config(route_name='new', renderer='new.mako')
 def new_view(request):
 	rs = request.db.execute('select root_url, name, id from sources')
@@ -114,7 +154,6 @@ def not_interesting(request):
 
 @view_config(route_name='delete_source')
 def delete_source(request):
-	print(request)
 	source_id = int(request.matchdict['id'])
 	request.db.execute('delete from sources where id = ?',
 		(str(source_id)))
@@ -158,6 +197,8 @@ def main():
 	config.include('pyramid_mako')
 	config.add_route('list', '/')
 	config.add_route('refresh', '/refresh')
+	config.add_route('settings', '/settings')
+	config.add_route('change_settings', '/change_settings')
 	config.add_route('read', '/read')
 	config.add_route('delete_source', '/delete_source/{id}')
 	config.add_route('new', '/new')
